ce6fcdb63094a6ded248a8593cc5405345fd131c
Han Xiao <han.xiao@cs.helsinki.fi>
2016-01-03 21:49:38 +0200

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='best_ukko_servers',
+      version='0.1',
+      description='Getting best ukko server names',
+      author='Han Xiao',
+      author_email='han.xiao@cs.helsinki.fi',
+      url='https://github.com/xiaohan2012/best_ukko_server',
+      scripts=['bin/best_ukko_servers']
+  )
